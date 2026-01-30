0b6db7596858e0c7235a28693da2ab557a61d8ed
rudolfo-felix <rudolfo.vmf@gmail.com>
2026-01-30 16:12:49 +0000

    ZD-147: reposition round status pill

--- src/lib/components/status-pill.svelte
+++ src/lib/components/status-pill.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div
-	class="absolute bottom-20 left-1/2 -translate-x-1/2 text-deep-teal px-4 py-2 rounded-full flex items-center gap-2 text-lg font-semibold text-center overflow-hidden"
+	class="absolute top-24 sm:top-28 md:top-32 left-1/2 -translate-x-1/2 z-30 max-w-[80vw] sm:max-w-[70vw] text-deep-teal px-4 py-2 rounded-full flex items-center gap-2 text-base md:text-lg font-semibold text-center overflow-hidden bg-white/90"
 >
 	{#if playerState.state === 'starting'}
 		<span> {m.choose_starting_stop()}</span>
